40d50bf49b07ac6363067a1bb163fb06aab79572
omsprog <omsprog@gmail.com>
2024-11-15 00:27:51 -0600

    refactorig TC Add Two Numbers

--- python-algorithms/test/test_add_two_numbers.py
+++ python-algorithms/test/test_add_two_numbers.py
@@ -1,12 +1,33 @@
 from src.add_two_numbers import AddTwoNumbers
 from src.add_two_numbers import ListNode
 
+def verifyNodeList(expectedList, listNode):
+    index = 0
+    while(listNode is not None):
+        assert listNode.val == expectedList[index]
+        index += 1
+        listNode = listNode.next
+
 def test_addTwoNumbers():
     aTN = AddTwoNumbers()
+    
+    #First 
+    listNode = aTN.addTwoNumbers(
+        ListNode(2, ListNode(4, ListNode(3, None))),
+        ListNode(5, ListNode(6, ListNode(4, None)))
+    )
+    verifyNodeList([7,0,8], listNode)
 
-    l1 = ListNode(2, ListNode(4, ListNode(3, None)))
-    l2 = ListNode(5, ListNode(6, ListNode(4, None)))
-    listNode = aTN.addTwoNumbers(l1, l2) # == [7,0,8]
-    while(listNode is not None):
-        print(listNode.val)
-        listNode = listNode.next
+    # Second
+    listNode = aTN.addTwoNumbers(
+        ListNode(0, None),
+        ListNode(0, None)
+    ) 
+    verifyNodeList([0], listNode)
+    
+    # Third
+    listNode = aTN.addTwoNumbers(
+        ListNode(9, ListNode(9, ListNode(9, ListNode(9, ListNode(9, ListNode(9, ListNode(9, None))))))),
+        ListNode(9, ListNode(9, ListNode(9, ListNode(9, None))))
+    ) 
+    verifyNodeList([8,9,9,9,0,0,0,1], listNode)
